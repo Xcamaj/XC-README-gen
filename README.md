dd49268c4925961cfbfed880bdcdc676b18ef10d
Xcamaj <Xhemajlcamaj@gmail.com>
2022-02-25 10:25:40 -0500

    data now gets pushed to readme file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# XC-README-gen
+
+    # x
+    # Description
+    x
+    #Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contact](#contact)
+    ## Installation
+    x
+    ## Usage 
+    x
+    ## License
+    Apache 2.0
+    ## Contact
+    * Github: x
+    * E-mail: x
+
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
+const questions = ([
     {
         type: 'input',
         name: 'Title',
@@ -58,34 +60,34 @@ const questions = inquirer.prompt([
                 console.log("Provide the user with instructions on how use the app!")
                 return false
             }
-        }  
+        }
     },
     {
         type: 'list',
         message: 'What license is being used?',
         name: 'License',
-        choices: ['Apache 2.0', 'BSD 3-Clause', 'GNU', 'MIT', 'Mozilla', 'Eclipse'],    
-         validate: value => {
+        choices: ['Apache 2.0', 'BSD 3-Clause', 'GNU', 'MIT', 'Mozilla', 'Eclipse'],
+        validate: value => {
             if (value) {
                 return true
             } else {
                 console.log("Please check what license was used!")
                 return false
             }
-        }  
+        }
     },
     {
         type: 'input',
         name: 'Username',
         message: 'Enter your Github username.',
-         validate: value => {
+        validate: value => {
             if (value) {
                 return true
             } else {
                 console.log("Please provide a username!")
                 return false
             }
-        }  
+        }
     },
     {
         type: 'input',
@@ -98,16 +100,40 @@ const questions = inquirer.prompt([
                 console.log("Please provide an email!")
                 return false
             }
-        }          
+        }
     }
 
 ]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+const writeToFile = fileName => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./README.md', fileName, err => {
+            if (err) {
+                reject(err)
+                return
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        })
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data){
+        console.log(data)
+        var fileName = generateMarkdown(data)
+        writeToFile(fileName)
+
+    })
+}
 
 //Function call to initialize app
 init();
+
+module.exports = questions
